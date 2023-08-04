0e505289f9c38432fb65e9550e016acbbcd5b374
TamaraSavadyan <savadyan.tamara@gmail.com>
2023-08-04 23:13:31 +0300

    change task1

--- task1_parser.py
+++ task1_parser.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import pandas as pd
-import time
-import requests 
-from bs4 import BeautifulSoup
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()

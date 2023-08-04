0344c231a6a7d4de5eddcc13105f252451c54179
TamaraSavadyan <savadyan.tamara@gmail.com>
2023-08-04 22:44:39 +0300

    init commit

---
+++ task1_parser.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import time
+import requests 
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
